8a1427a088ab17e9376f8c3a4ee22f97c0fa1b75
adamklockars <adamklockars@gmail.com>
2012-04-30 02:23:45 -0400

    fix blank player name issue

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -10,54 +10,47 @@ def init(request):
 
 def start(request):
     g = Game.objects.create()
-    # g.address.add(request.META['HTTP_X_FORWARDED_FOR'] or request.META.get['REMOTE_ADDR'])
-    g.player1=request.POST['player1']
-    g.piece1=request.POST['piece1']
-    g.player2=request.POST['player2']
+    g.player1 = request.POST['player1'] if not request.POST['player1'] == '' else request.POST['piece1']
+    g.piece1 = request.POST['piece1']
+    g.player2 = request.POST['player2']
     if request.POST['piece1'] == 'X':
-        g.piece2='O'
-        g.last_move='O'
+        g.player2 = request.POST['player2'] if not request.POST['player2'] == '' else 'O'
+        g.piece2 = 'O'
+        g.last_move = 'O'
     else:
-        g.piece2='X'
-        g.last_move='X'
+        g.player2 = request.POST['player2'] if not request.POST['player2'] == '' else 'X'
+        g.piece2 = 'X'
+        g.last_move = 'X'
     g.save()
     return HttpResponseRedirect(reverse('tictactoe.views.game', args=(g.id,)))
 
 def start_comp(request):
     g = Game.objects.create()
-    # g.address.add(request.META.get('HTTP_X_FORWARDED_FOR', '') or request.META.get('REMOTE_ADDR'))
-    g.player1=request.POST['player1']
-    g.piece1=request.POST['piece1']
-    g.player2='Computer'
+    g.player1 = request.POST['player1'] if not request.POST['player1'] == '' else request.POST['piece1']
+    g.piece1 = request.POST['piece1']
+    g.player2 = 'Computer'
     if request.POST['piece1'] == 'X':
-        g.piece2='O'
-        g.last_move='O'
+        g.piece2 = 'O'
+        g.last_move = 'O'
     else:
-        g.piece2='X'
-        g.last_move='X'
+        g.piece2 = 'X'
+        g.last_move = 'X'
     g.save()
     return HttpResponseRedirect(reverse('tictactoe.views.game', args=(g.id,)))
 
 def game(request, game_id):
     g = get_object_or_404(Game, pk=game_id)
-
     piece_turn = 'X' if g.last_move == 'O' else 'O'
     player_turn = g.player1 if g.piece2 == g.last_move else g.player2
-
     open_moves = g.get_open_moves()
     board = g.get_board()
     winner = g.get_winner()
+
     if winner == g.piece1:
-         if g.player1 == '':
-             winner = g.piece1
-         else:
-             winner = g.player1
+         winner = g.player1
          g.winner = g.piece1
     elif winner == g.piece2:
-         if g.player2 == '':
-             winner = g.piece2
-         else:
-             winner = g.player2
+         winner = g.player2
          g.winner = g.piece2
     g.save()
 
