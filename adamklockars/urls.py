230d1c925848293280575e66e164b35a68a6ea53
adamklockars <adamklockars@gmail.com>
2012-07-10 23:48:41 -0400

    start projects/blog

--- adamklockars/urls.py
+++ adamklockars/urls.py
@@ -7,6 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'adamklockars.views.home', name='home'),
     url(r'^bootstrap', 'adamklockars.views.bootstrap', name='bootstrap'),
+    url(r'^projects', 'adamklockars.views.projects', name='projects'),
+    url(r'^blog', 'adamklockars.views.blog', name='blog'),
     (r'^polls/', include('polls.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^tictactoe/', include('tictactoe.urls')),
--- adamklockars/views.py
+++ adamklockars/views.py
@@ -7,3 +7,11 @@ def home(request):
 @render_to('hero.html')
 def bootstrap(request):
     return {}
+
+@render_to('projects.html')
+def projects(request):
+    return {}
+
+@render_to('blog.html')
+def blog(request):
+    return {}
