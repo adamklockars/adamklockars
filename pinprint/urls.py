b4ba6d4971fb5a7756e42a55c5b642d2517983eb
adamklockars <adamklockars@gmail.com>
2012-08-11 23:42:36 -0400

    fix view

--- pinprint/urls.py
+++ pinprint/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^$', 'adamklockars.views.login', name='login'),
-    url(r'^account', 'adamklockars.views.account', name='account'),
-    url(r'^collage', 'adamklockars.views.collage', name='collage')
+    url(r'^$', 'pinprint.views.login', name='login'),
+    url(r'^account', 'pinprint.views.account', name='account'),
+    url(r'^collage', 'pinprint.views.collage', name='collage')
 )
--- pinprint/views.py
+++ pinprint/views.py
@@ -1,13 +1,13 @@
 from annoying.decorators import render_to
 
-@render_to('login.html')
+@render_to('pinprint/login.html')
 def login(request):
     return {}
 
-@render_to('account.html')
+@render_to('pinprint/account.html')
 def account(request):
     return {}
 
-@render_to('collage.html')
+@render_to('pinprint/collage.html')
 def collage(request):
     return {}
